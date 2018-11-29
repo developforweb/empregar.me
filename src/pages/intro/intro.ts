f7dbc3661eed74e2f10606e1368ceaf26fcac72f
DESKTOP-58NOIJJ\stylo <andersusilva@gmail.com>
2018-11-29 06:18:47 -0200

    Ultimas alterações da madrugada

--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -12,6 +12,23 @@ export class IntroPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
+  slides = [
+    {
+      title: "FACILIDADE DE USO!",
+      description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
+      image: "assets/imgs/slide1.jpg",
+    },
+    {
+      title: "DIVERSAS OPÇÕES!",
+      description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+      image: "assets/imgs/slide2.jpg",
+    },
+    {
+      title: "RECOLOCAÇÃO!",
+      description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+      image: "assets/img/slide3.jpg",
+    }
+  ];
 
   ionViewDidLoad() {
   }
